ae184af34ae88deb56048a28a5ea7d9616d60322
ProgramCodeRunner <114286340+ProgramCodeRunner@users.noreply.github.com>
2023-11-13 12:46:44 +0500

    fibonacci

--- zad1/main.py
+++ zad1/main.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 
-
-def fibonacci(n):
-    if n in (1, 2):
-        return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
-
-
 if __name__ == '__main__':
     print("Введите количество элементов:")
     n = int(input())
     massY = [0] * n
     massX = [0] * n
-    for i in range(n):
-        massY[i] = fibonacci(i + 1)
-        massX[i] = i+1
+
+    massY[0] = 1
+    massY[1] = 1
+    massX[0] = 1
+    massX[1] = 2
+
+    i = 2
+    while i < n:
+        massY[i] = massY[i - 1] + massY[i - 2]
+        massX[i] = i + 1
+        i += 1
 
     print(n)
     print(massY)
